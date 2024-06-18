d030b65411d6cfcb6203edf71d5d51ad7b83380c
Josias Silva <josilva1233@gmail.com>
2024-06-18 12:02:18 -0300

    incluindo a função de redirecionamento

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ document.getElementById('messageForm').addEventListener('submit', function(event
             alert('Mensagem enviada com sucesso!');
             form.reset(); // Limpa o formulário após o envio bem-sucedido, se necessário
             // Pode adicionar aqui qualquer ação após o envio bem-sucedido
+            window.location.href = 'https://josilva1233.github.io/myPage/';
         })
         .catch(function(error) {
             alert('Erro ao enviar mensagem. Por favor, tente novamente mais tarde.');
