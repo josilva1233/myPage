802fdd6067ca32a656304e4d152f2ce9a29ae5bc
Josias Silva <93230957+josilva1233@users.noreply.github.com>
2025-08-22 10:03:24 -0300

    Update main.js
    correções de performace

--- js/main.js
+++ js/main.js
@@ -19,9 +19,8 @@ document.getElementById('messageForm').addEventListener('submit', function(event
         })
         .then(function(data) {
             alert('Mensagem enviada com sucesso!');
-            form.reset(); // Limpa o formulário após o envio bem-sucedido, se necessário
-            // Pode adicionar aqui qualquer ação após o envio bem-sucedido
-            window.location.href = 'https://josilva1233.github.io/myPage/';
+            form.reset(); // Limpa o formulário após o envio bem-sucedido
+            // window.location.href = 'https://josilva1233.github.io/myPage/'; // Descomente se quiser redirecionar
         })
         .catch(function(error) {
             alert('Erro ao enviar mensagem. Por favor, tente novamente mais tarde.');
